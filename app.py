8f8a3af37bf4a695e0df801db0720ab15e40f8ca
Камила Колпашникова Kamila Kolpashnikova <kamilakolpashnikova@gmail.com>
2021-06-24 21:06:30 +0100

    new draft

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, redirect
 import os
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField
+from wtforms import StringField, SubmitField, DateField, SelectField
 from wtforms.validators import DataRequired
 import simplejson as json
 import datetime
 import bokeh
+from bokeh.io import curdoc
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.resources import CDN
@@ -17,7 +18,7 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 Bootstrap(app)
 
-def make_graph(ticker, month, year):
+def stock_graph(ticker, price_type, month, year):
     url = ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY"
     + "&symbol="
     + ticker
@@ -40,13 +41,23 @@ def make_graph(ticker, month, year):
     prices = []
     for date in dates:
         date_string = date.strftime('%Y-%m-%d')
-        price = full_data['Time Series (Daily)'][date_string]['4. close']
+        if price_type=="open":
+        	price = full_data['Time Series (Daily)'][date_string]['1. open']
+        elif price_type=="high":
+        	price = full_data['Time Series (Daily)'][date_string]['2. high']
+        elif price_type=="low":
+        	price = full_data['Time Series (Daily)'][date_string]['3. low']
+        elif price_type=="close":
+        	price = full_data['Time Series (Daily)'][date_string]['4. close']
         price = float(price)
         prices.append(price)
 
-    t = ("Closing Prices of " + ticker + " for the month of " + month + " "
+    tools = 'pan,wheel_zoom,xbox_select,reset'
+
+    t = (price_type.capitalize() + " Prices of " + ticker + " for the month of " + month + " "
          + year)
-    p = figure(x_axis_type="datetime", title=t)
+    curdoc().theme = 'dark_minimal'
+    p = figure(x_axis_type="datetime", title=t, width=600, height=600, tools=tools)
     p.grid.grid_line_alpha=0.3
     p.xaxis.axis_label = 'Date'
     p.yaxis.axis_label = 'Price'
@@ -57,9 +68,10 @@ def make_graph(ticker, month, year):
     return(p)
 
 class StockForm(FlaskForm):
-    ticker = StringField('Enter stock ticker code (e.g., AAPL):', validators=[DataRequired()])
+    ticker = StringField('Enter stock ticker code (e.g., AAPL, C, HPQ, INTC):', validators=[DataRequired()])
     month = DateField('Enter month (MM/YYYY):', format='%m/%Y', validators=[DataRequired()])
-    submit = SubmitField('Display Closing Price')
+    price_type = SelectField('Price Type', choices=[('open', 'Open'), ('high', 'High'), ('low', 'Low'), ('close', 'Close')])
+    submit = SubmitField('Graph Price')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -68,11 +80,12 @@ def index():
     if form.validate_on_submit():
         ticker = form.ticker.data
         date = form.month.data
+        price_data = form.price_type.data
         month = date.strftime('%B')
         year = date.strftime('%Y')
-        p = make_graph(ticker, month, year)
+        p = stock_graph(ticker, price_data, month, year)
         if p == 1:
-            output = "Unable to obtain data. Invalid stock ticker?"
+            output = "Wrong input. Try to check your stock ticker name."
         else:
             script, div = components(p)
             output = script + div
