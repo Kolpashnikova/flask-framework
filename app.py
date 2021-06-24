0680ebfb518c3fc92a33e3de8244489137df8c27
Камила Колпашникова Kamila Kolpashnikova <kamilakolpashnikova@gmail.com>
2021-06-24 16:10:27 +0100

    methods

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST']) 
+
 @app.route('/')
 def index():
   return render_template('index.html')
