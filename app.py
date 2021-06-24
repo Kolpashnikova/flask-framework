1ec4c8294cf98bc7afd897f7bbc11f361d9dd3e6
Камила Колпашникова Kamila Kolpashnikova <kamilakolpashnikova@gmail.com>
2021-06-24 19:03:17 +0100

    test gitignore

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  key = os.environ
+  return render_template('index.html', key=key)
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
