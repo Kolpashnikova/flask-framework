2b08f83ed157ed666abf620e082e2cb1ec2fdafb
Камила Колпашникова Kamila Kolpashnikova <kamilakolpashnikova@gmail.com>
2021-06-24 16:13:54 +0100

    adding get and post methods

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST']) 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
   return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
   return render_template('about.html')
 
